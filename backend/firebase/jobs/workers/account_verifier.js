e6b5b1e6eadd9a9a402226b53c86cc26b498e63c
Kapil Sachdeva <ksachdeva17@gmail.com>
2016-04-27 09:50:00 -0500

    feat(backend) - adjust the account_verifier worker to perform emailVerification only if App requires it and use whiteListedDomains if it requires

--- backend/firebase/jobs/workers/account_verifier.js
+++ backend/firebase/jobs/workers/account_verifier.js
@@ -1,9 +1,15 @@
 const Firebase = require('firebase');
 const crypto = require('crypto');
+const _ = require('lodash');
 const mailer = require('./../mailer');
 const config = require('./../config');
 
 const fb = new Firebase(config.FB_DB);
+
+const getDomainFromEmail = (email) => {
+  const email_string_array = email.split("@");
+  return email_string_array[email_string_array.length - 1];
+};
 
 const generateUniqueCode = () => {
   const chars = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
@@ -19,44 +25,71 @@ const generateUniqueCode = () => {
   return value.join('');
 };
 
-fb.authWithCustomToken(config.FB_MASTER_SECRET).then((authData) => {
+const startProcess = (whiteListDomains) => {
 
-  // listen for child_added event in the users
-  fb.child('users').on('child_added', (snapShot) => {
-    // check if we have a corresponding verification entry
-    const uid = snapShot.val().uid;
-    const email = snapShot.val().email;
+  const domains = whiteListDomains.split(';');
 
-    fb.child(`meta/users/${uid}`).once('value').then((metaSnapShot) => {
-      // if the node does not exists then create one
-      if (metaSnapShot.val() === null) {
-        const generatedCode = generateUniqueCode();
-        // we ought to create it
-        fb.child(`meta/users/${uid}`).set({
-          verified: false,
-          code: generatedCode
-        }).then(() => mailer.sendEmailVerification(generatedCode, email));
-      }
-    });
-  });
+  fb.authWithCustomToken(config.FB_MASTER_SECRET).then((authData) => {
 
-  // listen for child_added event in code_verifier/users
-  fb.child('code_verifier').on('child_added', (snapShot) => {
-    // compare the code with the one that we sent out earlier
-    const supplied_code_value = snapShot.val().code;
-    const entryKey = snapShot.key();
+    // listen for child_added event in the users
+    fb.child('users').on('child_added', (snapShot) => {
+      // check if we have a corresponding verification entry
+      const uid = snapShot.val().uid;
+      const email = snapShot.val().email;
 
-    fb.child(`meta/users/${entryKey}`).once('value').then((metaSnapShot) => {
-      const generated_code_value = metaSnapShot.val().code;
-      if (supplied_code_value === generated_code_value) {
-        // set verified to true
-        fb.child(`meta/users/${entryKey}`).set({
-          verified: true
+      // verify if the supplied email/domain is in our white List
+      if (_.includes(domains, getDomainFromEmail(email))) {
+        fb.child(`meta/users/${uid}`).once('value').then((metaSnapShot) => {
+          // if the node does not exists then create one
+          if (metaSnapShot.val() === null) {
+            const generatedCode = generateUniqueCode();
+            // we ought to create it
+            fb.child(`meta/users/${uid}`).set({
+              verified: false,
+              code: generatedCode
+            }).then(() => mailer.sendEmailVerification(generatedCode, email));
+          }
         });
-        // destroy the code_verifier node
-        fb.child(`code_verifier/${entryKey}`).remove();
+      } else {
+        // what should we do ?
+        console.warn(`An email ${email} that is not whiteListed was used ..`);
       }
     });
+
+    // listen for child_added event in code_verifier/users
+    fb.child('code_verifier').on('child_added', (snapShot) => {
+      // compare the code with the one that we sent out earlier
+      const supplied_code_value = snapShot.val().code;
+      const entryKey = snapShot.key();
+
+      fb.child(`meta/users/${entryKey}`).once('value').then((metaSnapShot) => {
+        const generated_code_value = metaSnapShot.val().code;
+        if (supplied_code_value === generated_code_value) {
+          // set verified to true
+          fb.child(`meta/users/${entryKey}`).set({
+            verified: true
+          });
+          // destroy the code_verifier node
+          fb.child(`code_verifier/${entryKey}`).remove();
+        }
+      });
+    });
+
   });
 
+};
+
+fb.authWithCustomToken(config.FB_MASTER_SECRET).then((authData) => {
+  fb.child('meta/app').once('value').then((appSnapShot) => {
+    const requireEmailVerification = appSnapShot.val().requireEmailVerification;
+
+    if (requireEmailVerification) {
+      console.info('Starting the worker ...');
+      startProcess(appSnapShot.val().whiteListDomains);
+    } else {
+      // we do not need really need this worker
+      console.info('Email verification is not required !');
+      process.exit(0);
+    }
+  });
 });
