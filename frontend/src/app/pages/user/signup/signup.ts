b27d0141b4dc205796fd7de33baf30b67af5a528
Kapil Sachdeva <ksachdeva17@gmail.com>
2016-04-28 12:40:40 -0500

    fix(frontend) - in signup, make sure to not allow registration if the email address entered is

--- frontend/src/app/pages/user/signup/signup.ts
+++ frontend/src/app/pages/user/signup/signup.ts
@@ -1,9 +1,10 @@
-import { Component} from 'angular2/core';
+import { Component, Inject} from 'angular2/core';
 import { Router, RouterLink } from 'angular2/router';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/common';
 import { Http } from 'angular2/http';
-import {AngularFire, FirebaseAuth} from 'angularfire2';
+import {AngularFire, FirebaseRef, FirebaseAuth} from 'angularfire2';
 import {encodeEmail} from './../../../utils';
+import * as _ from 'lodash';
 
 const template = require('./signup.html');
 
@@ -18,6 +19,7 @@ export class SignupPage {
   errorMessage: string;
 
   constructor(
+    @Inject(FirebaseRef) private fb: Firebase,
     private af: AngularFire,
     private fbAuth: FirebaseAuth,
     public router: Router,
@@ -26,34 +28,54 @@ export class SignupPage {
     this.invalidCredentials = false;
   }
 
+  getDomainFromEmail(email) {
+    const email_string_array = email.split('@');
+    return '@' + email_string_array[email_string_array.length - 1];
+  };
+
   signup(event, name, username, password, passwordAgain) {
     event.preventDefault();
 
     this.invalidCredentials = false;
 
-    this.fbAuth.createUser({
-      email: username,
-      password
-    })
-      .then((authData) => {
-        return this.fbAuth.login({
-          email: username,
-          password
-        });
-      })
-      .then((authData) => {
-        return this.af.object('/users/' + authData.uid).set({
-          name,
-          email: username,
-          uid: authData.uid,
-          verified: false,
-          verification_in_progress: false
-        });
-      })
-      .then(authData => this._onSuccessfullSignup())
+    this.fb.child('meta/app').once('value').then((snapShot) => {
+
+      const requireEmailVerification = snapShot.val().requireEmailVerification;
+      if (requireEmailVerification) {
+        const domains = snapShot.val().whiteListDomains;
+
+        if (!_.includes(domains, this.getDomainFromEmail(username))) {
+          throw {
+            code: 'INVALID_EMAIL_DOMAIN'
+          };
+        }
+      }
+
+      return this.fbAuth.createUser({
+        email: username,
+        password
+      });
+
+    }).then((authData) => {
+      return this.fbAuth.login({
+        email: username,
+        password
+      });
+    }).then((authData) => {
+      return this.af.object('/users/' + authData.uid).set({
+        name,
+        email: username,
+        uid: authData.uid,
+        verified: false,
+        verification_in_progress: false
+      });
+    }).then(authData => this._onSuccessfullSignup())
       .catch((error) => {
         this.invalidCredentials = true;
         switch (error.code) {
+          case 'INVALID_EMAIL_DOMAIN':
+            this.errorMessage = `This email address is not allowed!.`;
+            break;
           case 'EMAIL_TAKEN':
             this.errorMessage = `The new user account cannot be created
               because the email is already in use.`;
